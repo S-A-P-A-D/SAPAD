bfc6758a0936f34d56a52aa4ab2336878d1c29a4
Felipe A de Souza <felipe.az.souza08@gmail.com>
2021-08-16 21:14:16 -0300

    Calendar contents

--- lib/Telas/HomeScreens/calendar.dart
+++ lib/Telas/HomeScreens/calendar.dart
@@ -17,6 +17,61 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return SfCalendar();
+    return SfCalendar(
+      view: CalendarView.month,
+      showNavigationArrow: true,
+    );
   }
+}
+
+List<CalendarEmote> _getDataSource() {
+  final List<CalendarEmote> calendarEmote = <CalendarEmote>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+  calendarEmote.add(CalendarEmote(
+      'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+  return calendarEmote;
+}
+
+class CalendarEmoteSource extends CalendarDataSource {
+  CalendarEmoteSource(List<CalendarEmote> source) {
+    appointments:
+    source;
+  }
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class CalendarEmote {
+  CalendarEmote(
+      this.eventName, this.from, this.to, this.background, this.isAllDay);
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }
